3b36ec2f125dc75eeea5202ea544edd4bdbdef1a
geumjue <whkrmawn@naver.com>
2025-07-29 18:30:09 +0900

    측정 후 페이지에서 자세히 보러가기-> 결과 페이지 연결

--- moodin_app/lib/home/home_view.dart
+++ moodin_app/lib/home/home_view.dart
@@ -3,6 +3,7 @@ import 'package:moodin_app/mypage/mypage_view.dart';
 import 'package:provider/provider.dart';
 import 'package:moodin_app/measure/measure_model.dart';
 import 'package:moodin_app/measure/measure_view.dart';
+import 'package:moodin_app/result/result_view.dart';
 
 class HomeView extends StatelessWidget {
   final String username;
@@ -97,7 +98,7 @@ class HomeView extends StatelessWidget {
                                           context,
                                           MaterialPageRoute(
                                               builder: (context) =>
-                                                  const MyPageView()),
+                                                  const ResultView()),
                                         );
                                       },
                                       child: const Text(

dfb2aadcada46f1dce254896e7d85242659048b5
munstate <ckdwnans@naver.com>
2025-09-02 20:38:02 +0900

    워치에서 데이터 받아오기

--- moodin_app/lib/main.dart
+++ moodin_app/lib/main.dart
@@ -6,9 +6,13 @@ import 'package:moodin_app/intro/intro_view.dart';
 import 'package:moodin_app/login/login_view.dart';
 import 'package:moodin_app/measure/measure_view.dart';
 import 'package:moodin_app/mypage/mypage_view.dart';
-import 'package:moodin_app/survey/survey_model.dart'; 
-import 'package:moodin_app/survey/survey_view.dart'; 
-import 'package:moodin_app/survey/survey_result_view.dart'; 
+import 'package:moodin_app/survey/survey_model.dart';
+import 'package:moodin_app/survey/survey_view.dart';
+import 'package:moodin_app/survey/survey_result_view.dart';
+import 'package:moodin_app/result/hrv_service.dart';
+import 'package:moodin_app/result/result_controller.dart';
+import 'package:moodin_app/result/result_view.dart';
+import 'package:moodin_app/result/stress_api.dart';
 
 void main() {
   runApp(
@@ -16,6 +20,12 @@ void main() {
       providers: [
         ChangeNotifierProvider(create: (_) => MeasureModel()),
         ChangeNotifierProvider(create: (_) => SurveyModel()),
+        ChangeNotifierProvider(
+          create: (_) => ResultController(
+            hrvService: HrvService(),
+            stressApi: StressApi("http://10.0.2.2:8080"), // 백엔드 주소
+          ),
+        ),
       ],
       child: const MyApp(),
     ),
@@ -36,7 +46,8 @@ class MyApp extends StatelessWidget {
         '/measure': (_) => const MeasureView(),
         '/home': (_) => const HomeView(username: '무딘'),
         '/mypage': (_) => const MyPageView(),
-        '/survey': (_) => const SurveyView(), // 설문조사 등록
+        '/survey': (_) => const SurveyView(),
+        '/result': (_) => const ResultView(), // 결과 화면 추가
       },
     );
   }
